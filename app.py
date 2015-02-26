0d45db1e06f9d54b35e1abd791a6253d311af3c7
Robert Rees <robert.rees@guardian.co.uk>
2015-02-26 15:49:06 +0000

    Creates the previous and next handlers

--- app.py
+++ app.py
@@ -27,6 +27,22 @@ class DayPage(webapp2.RequestHandler):
 
 		self.response.out.write(template.render(template_values))
 
+def new_date_string(date, days=1):
+	current_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+	new_date = current_date + datetime.timedelta(days=days)
+	return new_date.strftime('%Y-%m-%d')
+
+class PreviousDay(webapp2.RequestHandler):
+	def get(self, date):
+		return webapp2.redirect('/day/{0}'.format(new_date_string(date, days=-1)))
+
+class NextDay(webapp2.RequestHandler):
+	def get(self, date):
+		return webapp2.redirect('/day/{0}'.format(new_date_string(date)))
+
 app = webapp2.WSGIApplication([
-	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>', handler=DayPage),],
+	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>', handler=DayPage),
+	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>/previous', handler=PreviousDay),
+	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>/next', handler=NextDay),
+	],
                               debug=True)
