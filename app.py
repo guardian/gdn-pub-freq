e5b6e6ed4a63f1ea62909c7bccfc8f9234636272
Robert Rees <robert.rees@guardian.co.uk>
2015-02-27 09:31:45 +0000

    Adds navigation links for days and weeks

--- app.py
+++ app.py
@@ -45,10 +45,25 @@ class NextDay(webapp2.RequestHandler):
 	def get(self, date):
 		return webapp2.redirect('/day/{0}'.format(new_date_string(date)))
 
+def new_week_string(date, days=7):
+	current_date = datetime.datetime.strptime(date, '%Y-%m-%d')
+	new_date = current_date + datetime.timedelta(days=days)
+	return new_date.strftime('%Y-%m-%d')
+
+class PreviousWeek(webapp2.RequestHandler):
+	def get(self, date):
+		return webapp2.redirect('/day/{0}'.format(new_week_string(date, days=-7)))
+
+class NextWeek(webapp2.RequestHandler):
+	def get(self, date):
+		return webapp2.redirect('/day/{0}'.format(new_week_string(date)))
+
 app = webapp2.WSGIApplication([
 	webapp2.Route(r'/', handler=MainPage),
 	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>', handler=DayPage),
 	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>/previous', handler=PreviousDay),
 	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>/next', handler=NextDay),
+	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>/previous/week', handler=PreviousWeek),
+	webapp2.Route(r'/day/<date:\d{4}-\d{2}-\d{2}>/next/week', handler=NextWeek),
 	],
                               debug=True)
